47672feef3f571036d9e8568739c4b1a69896dd2
Elena Morilova <elena.morilova@mail.ru>
2023-04-24 20:16:59 +0400

    I can do it!

---
+++ work_with_list_test.py
@@ -0,0 +1,27 @@
+from itertools import compress
+
+def test_list_cicle():
+  a = [1, 4, 2, 3, 5, 8, 10, 11, 4, 6, 7]
+  b=[elem for elem in a if elem < 6]
+  print(b)
+  assert [1, 4, 2, 3, 4]
+
+  #print(*[a for a in [1, 4, 2, 3, 5, 8, 10, 11, 4, 6, 7] if a < 6])
+  #print([elem for elem in a if elem < 6])
+
+def test_list_filter():
+    a = [1, 4, 2, 3, 5, 8, 10, 11, 4, 6, 7]
+    print(*filter(lambda x: x < 6, a))
+
+
+def test_list_comprehensions():
+    a = [1, 4, 2, 3, 5, 8, 10, 11, 4, 6, 7]
+    print(*compress(a, map(lambda x: x < 6, a)))
+
+def test_palindrome():
+
+    a = input('madam')
+    if str(a) == str(a)[::-1]:
+        print("Palindrome")
+    else:
+        print("Not Palindrome")
